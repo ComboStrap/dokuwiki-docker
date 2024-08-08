4e0ec8a33f2d22e6ac4695636e6ae74980faa8f5
Nico <gerardnico@gmail.com>
2024-08-08 18:25:13 +0200

    Docker Dokuwiki on the run

--- resources/dokuwiki/dokuwiki-docker/conf/default.php
+++ resources/dokuwiki/dokuwiki-docker/conf/default.php
@@ -3,16 +3,3 @@
  * Default Values
  */
 
-/**
- * URL rewrite is on
- * https://www.dokuwiki.org/config:im_convert
- */
-$conf['userewrite'] = 1;
-// `use slash` is needed with user rewrite
-$conf['useslash'] = 1;
-
-/**
- * Gd
- * https://www.dokuwiki.org/config:im_convert
- */
-$conf['im_convert'] = '/usr/bin/convert';
--- resources/dokuwiki/dokuwiki-docker/conf/protected.php
+++ resources/dokuwiki/dokuwiki-docker/conf/protected.php
@@ -2,5 +2,18 @@
 /**
  * https://github.com/dokuwiki/docker/blob/main/root/var/www/html/conf/docker.protected.php
  * settings that should not be overwritten in the docker environment
- * https://www.dokuwiki.org/config:savedir
  */
+
+/**
+ * URL rewrite is on
+ * https://www.dokuwiki.org/config:im_convert
+ */
+$conf['userewrite'] = 1;
+// `use slash` is needed with user rewrite
+$conf['useslash'] = 1;
+
+/**
+ * Gd
+ * https://www.dokuwiki.org/config:im_convert
+ */
+$conf['im_convert'] = '/usr/bin/convert';
