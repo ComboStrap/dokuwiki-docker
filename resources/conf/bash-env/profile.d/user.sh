6203a3a854edc4a15ab67c8a2f23f0f774ea66f1
Nicolas GERARD <gerardnico@gmail.com>
2024-09-27 17:17:00 +0200

    Added env

--- resources/conf/bash-env/profile.d/user.sh
+++ resources/conf/bash-env/profile.d/user.sh
@@ -4,10 +4,9 @@
 # This script is called by `/etc/profile`
 # Run on non-interactive, ie when the terminal starts
 
-# Note: PS1 and umask are already set in /etc/profile. You should not
-# need this unless you want different defaults for root.
-# PS1='${debian_chroot:+($debian_chroot)}\h:\w\$ '
-# umask 022
+# Note we can also fake it with https://cwrap.org/nss_wrapper.html
+# See example: https://github.com/docker-library/postgres/pull/448/files
+
 USER_UID=$(id -u)
 USER_NAME=$(whoami 2> /dev/null || echo 'noname')
 echo_info "User: $USER_NAME, UID: $USER_UID, GID: $(id -g)"
